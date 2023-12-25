ac1aab8b833c90448fc343e5baaf91a07dc0846c
narottambhardwaj10062000 <narottam.kumarbhardwaj.ece23@heritageit.edu.in>
2023-12-25 20:01:08 +0530

    starter files ready

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>This Is A Demo Project To Explain React Context API</h1>
+      <input 
+        type="text"
+        placeholder="UserName"
+      />
+      <input 
+        type="text"
+        placeholder="Email"
+      />
+      <button>Submit</button>
+    </>
+  )
+}
+
+export default App
